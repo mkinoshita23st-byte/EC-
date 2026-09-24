a9882ee132b00c313513c1b0b31e033cbf5b413e
izumi <izumi@kkkkk>
2026-09-24 11:05:09 +0900

    DAO.CardのisMain処理を追加

--- src/main/java/DAO/Card.java
+++ src/main/java/DAO/Card.java
@@ -45,14 +45,20 @@ public class Card {
 	}
 	//カード更新
 	public void updateCard(int id,int cardId,String number,
-			String expiry,String holder,Boolean isMain) {
+			String expiry,String holder,Boolean isMain) {//sessionのcardListが必要？
 		try {
 			Context context=new InitialContext();
 			DataSource ds=(DataSource)context.lookup(
 					"java:comp/env/jdbc/abcdpool");
 			try (Connection db=ds.getConnection()){
+				if(isMain) {
+					PreparedStatement psIsMain=db.prepareStatement(
+							"UPDATE usr_cards SET is_main = false WHERE id = ?");
+							psIsMain.setInt(1, id);
+							psIsMain.executeUpdate();
+				}
 				PreparedStatement ps=db.prepareStatement(
-						"UPDATE usr_cards SET number=?,"
+						"UPDATE usr_cards SET card_id=?,number=?,"
 						+ "expiry=?,holder=?,is_main=?"
 						+ " WHERE id = ?;");
 	            ps.setInt(1,cardId);
@@ -61,61 +67,39 @@ public class Card {
 	            ps.setString(4,holder);
 	            ps.setBoolean(5,isMain);
 	            ps.setInt(6,id);
-	            if(isMain) {
-	            	ps=db.prepareStatement("UPDATE usr_cards SET is_main = false WHERE id = ?;");
-	            }
 	            ps.executeUpdate();
 			}//catch略
 		}catch(NamingException | SQLException e){
 			e.printStackTrace();
 		}
-	}
-      
-            try {
-                // 更新対象がメインカードに指定された場合、対象ユーザーの全カードを先にfalseへリセット
-                if (Boolean.TRUE.equals(isMain)) {
-                    try (PreparedStatement psReset = db.prepareStatement(resetMainSql)) {
-                        psReset.setInt(1, id);
-                        psReset.executeUpdate();
-                    }
-                }
+      }
 
-
-    // カード新規追加（isMainがtrueの場合、同ユーザーの他カードを自動的にfalseへ更新）
+    // カード新規追加
     public void addCard(int id, String number, String expiry, String holder, Boolean isMain) {
-        String resetMainSql = "UPDATE usr_cards SET is_main = false WHERE id = ?;";
-        String insertSql = "INSERT INTO usr_cards (id, number, expiry, holder, is_main) VALUES (?, ?, ?, ?, ?);";
-
-        try (Connection db = getConnection()) {
-            db.setAutoCommit(false); // トランザクション開始
-
-            try {
-                // 新規カードがメインに指定された場合、対象ユーザーの既存カードをすべてfalseにリセット
-                if (Boolean.TRUE.equals(isMain)) {
-                    try (PreparedStatement psReset = db.prepareStatement(resetMainSql)) {
-                        psReset.setInt(1, id);
-                        psReset.executeUpdate();
-                    }
-                }
-
-                // 新しいカード情報を登録
-                try (PreparedStatement ps = db.prepareStatement(insertSql)) {
-                    ps.setInt(1, id);
-                    ps.setString(2, number);
-                    ps.setString(3, expiry);
-                    ps.setString(4, holder);
-                    ps.setBoolean(5, isMain);
-                    ps.executeUpdate();
-                }
-
-                db.commit(); // トランザクション完了
-            } catch (SQLException e) {
-                db.rollback(); // エラー時はロールバック
-                throw e;
-            }
-        } catch (NamingException | SQLException e) {
-            e.printStackTrace();
-        }
+    	try {
+			Context context=new InitialContext();
+			DataSource ds=(DataSource)context.lookup(
+					"java:comp/env/jdbc/abcdpool");
+			try (Connection db=ds.getConnection()){
+				if(isMain) {
+					PreparedStatement psIsMain=db.prepareStatement(
+							"UPDATE usr_cards SET is_main = false WHERE id = ?");
+							psIsMain.setInt(1, id);
+							psIsMain.executeUpdate();
+				}
+				PreparedStatement ps=db.prepareStatement(
+						"INSERT INTO usr_cards (id, number, expiry, holder, is_main) "
+						+ "VALUES (?, ?, ?, ?, ?);");
+	            ps.setString(1,number);
+	            ps.setString(2,expiry);
+	            ps.setString(3,holder);
+	            ps.setBoolean(4,isMain);
+	            ps.setInt(5,id);
+	            ps.executeUpdate();
+			}//catch略
+		}catch(NamingException | SQLException e){
+			e.printStackTrace();
+		}
     }
 }
 
